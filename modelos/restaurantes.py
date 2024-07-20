6614ad004102902cbdc477ee62360c86825641ed
EnzoClares <enzoclares1712@gmail.com>
2024-07-20 11:35:40 -0300

    criando métodos

--- modelos/restaurantes.py
+++ modelos/restaurantes.py
@@ -1,18 +1,22 @@
-class restaurante:
+class Restaurante:
+  restaurantes = []
+
   def __init__(self, nome, categoria):
       self.nome = nome
       self.categoria = categoria
       self.ativo = False
+      Restaurante.restaurantes.append(self)
   def __str__(self):
       return f'{self.nome} | {self.categoria} | {self.ativo}'
+  
+  def listar_restaurantes():
+      for restaurante in Restaurante.restaurantes:
+          print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 '''self é a referência da instância atual que está chamando a classe'''
 
-restaurante_praça = restaurante('Praça', 'Comida italiana')
+restaurante_praça = Restaurante('Praça', 'Comida italiana')
 
-restaurante_pizza = restaurante('Pizza express', 'Comida italiana')
+restaurante_pizza = Restaurante('Pizza express', 'Comida italiana')
 
-restaurantes = [restaurante_praça, restaurante_pizza]
-
-print(restaurante_praça)
-print(restaurante_pizza)
+Restaurante.listar_restaurantes()
