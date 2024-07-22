116e21455c88d76c171b1b0d09de8d68bb588704
EnzoClares <enzoclares1712@gmail.com>
2024-07-22 11:19:29 -0300

    uso de title(), upper() e _ para privar

--- modelos/restaurantes.py
+++ modelos/restaurantes.py
@@ -2,17 +2,17 @@ class Restaurante:
   restaurantes = []
 
   def __init__(self, nome, categoria):
-      self.nome = nome
-      self.categoria = categoria
+      self._nome = nome.title()
+      self._categoria = categoria.upper()
       self._ativo = False
       Restaurante.restaurantes.append(self)
   def __str__(self):
-      return f'{self.nome} | {self.categoria}'
+      return f'{self._nome} | {self._categoria}'
   
   def listar_restaurantes():
       print(f'{'nome do restaurante'.ljust(25)} | {'categoria'.ljust(25)} | {'status'}')
       for restaurante in Restaurante.restaurantes:
-          print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+          print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
 
   @property
   def ativo(self):
@@ -22,7 +22,7 @@ class Restaurante:
 
 restaurante_praça = Restaurante('Praça', 'Comida italiana')
 
-restaurante_pizza = Restaurante('Pizza express', 'Comida italiana')
+restaurante_pizza = Restaurante('pizza express', 'Comida italiana')
 
 Restaurante.listar_restaurantes()
 
